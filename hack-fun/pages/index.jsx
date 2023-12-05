3d04d4c47da17b0ee3191cf6bfb923a31488970f
Selam Ararsa <selamkedir@icloud.com>
2023-12-05 13:36:03 +0000

    global styles directory

--- hack-fun/pages/index.jsx
+++ hack-fun/pages/index.jsx
@@ -1,9 +1,11 @@
 import LeftSideBar from '../src/app/components/LeftSideBar';
 import HomeIntro from '../src/app/components/HomeIntro';
 import Homepage from '../src/app/components/HomeCard';
+// import '../styles/globals.css';
 const Home = () => {
   return (
-    <div>
+    <div className="flex">
+      <LeftSideBar />
       <HomeIntro />
       <Homepage />
     </div>
