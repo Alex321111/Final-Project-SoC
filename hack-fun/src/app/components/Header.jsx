0302be8543d18620303749f233352bfc36ad1d75
ZoeBrooks539 <zmbfood72@gmail.com>
2023-12-05 12:03:40 +0000

    home page intro

--- hack-fun/src/app/components/Header.jsx
+++ hack-fun/src/app/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header(){
 
 
-    <img logo href="" > </img>
+    <img logo class = "image"  href="https://pbs.twimg.com/profile_images/1584960450030075904/Y_b5YOBE_400x400.jpg" > </img>
     <img logo href= ""></img>
     <img avatar href= ""></img>
     <h2> Welcome to Hack-a-fun!</h2>
---
+++ hack-fun/src/app/components/HomeIntro.jsx
@@ -0,0 +1,8 @@
+function HomeIntro(){
+<h2>Welcome to Hack-a-fun!</h2>
+
+<p> Welcome to the Hack-a-fun app. If you are reading this you, then you are about to take part in hackathons with a difference!
+    This app is run in conjuction with School of Code and is open to all current and past Bootcampers.
+    Here you will work with a randomised group  </p>
+
+}
