57939ed7822fe9a1aa55e475a7497c658638b960
Julian Ngo <julian@Julians-iMac.local>
2023-12-05 12:02:50 +0000

    HomeCard changes

--- hack-fun/src/app/components/HomeCard.jsx
+++ hack-fun/src/app/components/HomeCard.jsx
@@ -1,5 +1,15 @@
-<section>
-  <p>Welcome to Hackafun</p>
-  <h2>Project Brief #1</h2>
-  <button>Sign up!</button>
-</section>;
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <section>
+      <p>Welcome to Hackafun</p>
+      <h2>Project Brief #1</h2>
+      <a href="*link to sign up page*">
+        <button>Sign up!</button>
+      </a>
+    </section>
+  );
+};
+
+export default Homepage;
