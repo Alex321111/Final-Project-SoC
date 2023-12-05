7c25e4d022d0377da13def95c41aad5b4cfbe3c6
Selam Ararsa <selamkedir@icloud.com>
2023-12-05 12:07:12 +0000

    routing between home and pages

--- hack-fun/pages/index.jsx
+++ hack-fun/pages/index.jsx
@@ -1,7 +1,9 @@
-// pages/index.js
+import LeftSideBar from '../src/app/components/LeftSideBar';
+
 const Home = () => {
   return (
     <div>
+      <LeftSideBar />
       <h1>Welcome to the Home Page</h1>
       <p>This is a test home component.</p>
     </div>
