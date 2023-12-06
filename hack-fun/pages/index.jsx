1a473643791a0ea8d3b9f4cf04ed9d37876765ff
Selam Ararsa <selamkedir@icloud.com>
2023-12-06 09:58:40 +0000

    tailwind config

--- hack-fun/pages/index.jsx
+++ hack-fun/pages/index.jsx
@@ -6,12 +6,20 @@ import Header from '../src/app/components/Header';
 import '../styles/globals.css';
 const Home = () => {
   return (
-    <div className="flex">
-      <Header />
-      <LeftSideBar />
-      <HomeIntro />
-      <HomeCard />
-      <Footer />
+    <div className="flex flex-col h-screen">
+      <Header className="fixed top-0 left-0 w-full" />
+
+      <div className="flex flex-grow">
+        <div className="left-side-bar">
+          <LeftSideBar />
+        </div>
+        <div className="flex flex-col flex-grow">
+          <section className="flex flex-col md:flex-grow">
+            <HomeIntro />
+            <HomeCard />
+          </section>
+        </div>
+      </div>
     </div>
   );
 };
--- hack-fun/tailwind.config.ts
+++ hack-fun/tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -7,14 +7,55 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'primary-500': '#877EFF',
+        'primary-600': '#5D5FEF',
+        'secondary-500': '#FFB620',
+        'off-white': '#D0DFFF',
+        'red': '#FF5A5A',
+        'dark-1': '#000000',
+        'dark-2': '#09090A',
+        'dark-3': '#101012',
+        'dark-4': '#1F1F22',
+        'light-1': '#FFFFFF',
+        'light-2': '#EFEFEF',
+        'light-3': '#7878A3',
+        'light-4': '#5C5C7B',
+      },
+      screens: {
+        xs: '480px',
+      },
+      width: {
+        '420': '420px',
+        '465': '465px',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
