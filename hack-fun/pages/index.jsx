6dfe7932908b47cb1a68270a0ee91d6a2c73747e
Selam Ararsa <selamkedir@icloud.com>
2023-12-06 11:24:05 +0000

    main div vh set to 100

--- hack-fun/pages/index.jsx
+++ hack-fun/pages/index.jsx
@@ -1,17 +1,21 @@
-import LeftSideBar from "../src/app/components/LeftSideBar";
-import HomeIntro from "../src/app/components/HomeIntro";
-import HomeCard from "../src/app/components/HomeCard";
-import Footer from "../src/app/components/Footer";
-import Header from "../src/app/components/Header";
-import BottomBar from "../src/app/components/BottomBar";
-import "../styles/globals.css";
+// Home.js
+import LeftSideBar from '../src/app/components/LeftSideBar';
+import HomeIntro from '../src/app/components/HomeIntro';
+import HomeCard from '../src/app/components/HomeCard';
+import Footer from '../src/app/components/Footer';
+import Header from '../src/app/components/Header';
+import BottomBar from '../src/app/components/BottomBar';
+import '../styles/globals.css';
 
 const Home = () => {
   return (
-    <div className="flex flex-col h-screen">
+    <div
+      className="flex flex-col min-h-screen border border-solid border-green-300"
+      style={{ minHeight: '100vh', border: '2px solid green' }}
+    >
       <Header className="fixed top-0 left-0 w-full" />
 
-      <div className="flex flex-grow">
+      <div className="flex flex-grow border border-solid border-green-300">
         <div className="left-side-bar">
           <LeftSideBar />
         </div>
@@ -22,10 +26,9 @@ const Home = () => {
           </section>
         </div>
       </div>
-      <div className="fixed bottom-0 w-full">
-        <div className="bottombar">
-          <BottomBar />
-        </div>
+
+      <div className="rounded-[10px] bg-primary-500 flex-center flex-col gap-1 p-2 transition">
+        <BottomBar />
       </div>
     </div>
   );
