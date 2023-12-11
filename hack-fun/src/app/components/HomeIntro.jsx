82aa6be7d8a6c809ea9dd4182137c95a3461d0ae
Julian Ngo <julian@Julians-iMac.local>
2023-12-11 15:01:40 +0000

    playing with tailwind

--- hack-fun/src/app/components/HomeIntro.jsx
+++ hack-fun/src/app/components/HomeIntro.jsx
@@ -1,21 +1,16 @@
 export default function HomeIntro() {
   return (
     <>
-      <h2 class="font-semibold text-blue-800 text-4xl pt-10">
-        Welcome to Hack-a-fun!
-      </h2>
+      <div class="border-4 border-blue-300 rounded-md mx-3">
+        <h2 class="font-bold text-green-400 text-4xl pt-10 text-center font-sans-arial">
+          Welcome to Hackafun!
+        </h2>
 
-      <p class="font-bold text-gray-500 text-2xl pt-10 pb-10">
-        Welcome to the Hack-a-fun app. You are about to take part in hackathons
-        with a difference! Here you will work with a randomised group of fellow
-        School of Code bootcampers, recent graduates and a mentor to solve a
-        Hackathon. New hackathons go live every 4 weeks. Sign up if you are
-        interested. You will then be assigned into a group. Your team will then
-        have access to the GitHub repo for that Hackathon. Once your team had
-        completed the hackathon you can Save it! Make a video about it! Add it
-        to your portfolio. Have fun and remember - it is all about working and
-        learning together!
-      </p>
+        <p class="font-sans-roboto text-gray-300 text-2xl py-10 px-5">
+          Welcome to the Hackafun app. You are about to take part in hackathons
+          with a difference, working with your fellow bootcampers and a mentor!
+        </p>
+      </div>
     </>
   );
 }
