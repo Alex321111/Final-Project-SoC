bb3effd1270345e600a1914d525eb180f3d7a4f9
Julian Ngo <julian@Julians-iMac.local>
2023-12-11 14:08:14 +0000

    styled h2 and p elements in HomeIntro

--- hack-fun/src/app/components/HomeIntro.jsx
+++ hack-fun/src/app/components/HomeIntro.jsx
@@ -1,19 +1,20 @@
 export default function HomeIntro() {
   return (
     <>
-      <h2>Welcome to Hack-a-fun!</h2>
+      <h2 class="font-semibold text-blue-800 text-4xl pt-10">
+        Welcome to Hack-a-fun!
+      </h2>
 
-      <p>
-        {' '}
-        Welcome to the Hack-a-fun app. You are about to take part in hackathons with a difference! 
-        Here you will work with a randomised group of fellow School of Code bootcampers, recent graduates and a mentor to solve a Hackathon. 
-        New hackathons go live every 4 weeks. 
-        Sign up if you are interested. 
-        You will then be assigned into a group. Your team will then have access to
-        the GitHub repo for that Hackathon. 
-        Once your team had completed the hackathon you can Save it! Make a video about it! Add it to your
-        portfolio. 
-        Have fun and remember - it is all about working and learning together!{' '}
+      <p class="font-bold text-gray-500 text-2xl pt-10 pb-10">
+        Welcome to the Hack-a-fun app. You are about to take part in hackathons
+        with a difference! Here you will work with a randomised group of fellow
+        School of Code bootcampers, recent graduates and a mentor to solve a
+        Hackathon. New hackathons go live every 4 weeks. Sign up if you are
+        interested. You will then be assigned into a group. Your team will then
+        have access to the GitHub repo for that Hackathon. Once your team had
+        completed the hackathon you can Save it! Make a video about it! Add it
+        to your portfolio. Have fun and remember - it is all about working and
+        learning together!
       </p>
     </>
   );
