235d0f266e7a9336d169dc875dce1a54e362a38a
Selam Ararsa <selamkedir@icloud.com>
2023-12-11 11:34:08 +0000

    left side bar

--- hack-fun/src/app/components/LeftSideBar.jsx
+++ hack-fun/src/app/components/LeftSideBar.jsx
@@ -1,32 +1,60 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import SocLogo from '../components/take-three.png';
+import Avatar from './Avatar.jsx';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faCircleStop } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHouse,
+  faCircleStop,
+  faComments,
+  faListCheck,
+  faDiagramProject,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
+
 const LeftSideBar = () => {
   return (
     <nav className="leftsidebar flex min-h-screen -mr-3.5">
       <div className="flex flex-col gap-11">
-        <img className="soc-logo" src="./take-three.png" />
+        <div className="flex flex-col items-center justfy-center">
+          <Image className="soc-logo" width={80} height={80} src={SocLogo} />
+        </div>
         <ul className="py-4 px-6">
           <Link href="/">
-            <li className="py-3">
+            <li className="py-3 hover:bg-primary-500  rounded-lg py-5">
               {' '}
               <FontAwesomeIcon icon={faHouse} className="px-2" /> Home
             </li>
           </Link>
           <Link href="/groupchat">
-            <li className="py-3">Community chat</li>
+            <li className="py-3 hover:bg-primary-500  rounded-lg py-5">
+              <FontAwesomeIcon icon={faComments} className="px-2" />
+              Community chat
+            </li>
           </Link>
           <Link href="/teamchat">
-            <li className="py-3">Team chat</li>
+            <li className="py-3 hover:bg-primary-500  rounded-lg px-2">
+              <FontAwesomeIcon icon={faComments} className="px-2" />
+              Team chat
+            </li>
           </Link>
           <Link href="/projectsubmissions">
-            <li className="py-3"> Project submissions </li>
+            <li className="py-3 hover:bg-primary-500 rounded-lg px-2">
+              <FontAwesomeIcon icon={faListCheck} className="px-2" />
+              Project submissions
+            </li>
           </Link>
           <Link href="/pastprojects">
-            <li className="py-3">Past projects </li>
+            <li className="py-3 hover:bg-primary-500  rounded-lg px-2">
+              <FontAwesomeIcon icon={faDiagramProject} className="px-2" />
+              Past projects{' '}
+            </li>
           </Link>
           <Link href="/profile">
-            <li className="py-3">Profile </li>
+            <li className="py-3 hover:bg-primary-500  rounded-lg px-2">
+              <FontAwesomeIcon icon={faUser} className="px-2" />
+              Profile{' '}
+            </li>
           </Link>
         </ul>
       </div>
