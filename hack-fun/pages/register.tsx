f01444770f4fa2944b61fd65b4bed8e01e6ac925
Selam Ararsa <selamkedir@icloud.com>
2023-12-12 13:57:48 +0000

    register page working

--- hack-fun/pages/register.tsx
+++ hack-fun/pages/register.tsx
@@ -1,7 +1,7 @@
 //Register page tsx
 'use client';
 import react from 'react';
-import { Input, Button } from '@supabase/ui';
+
 import supabase from '../src/app/utils/supabase';
 export default function Register() {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -48,31 +48,41 @@ export default function Register() {
   return (
     <div className="mx-auto flex min-h-screen max-w-2xl items-center px-4">
       <form className="w-full space-y-2" onSubmit={handleSubmit}>
-        <Input type="email" name="email" label="Email" />
-        <Input type="password" name="password" label="Password" />
-        <Input type="username" name="username" label="username" />
-        <Input type="name" name="name" label="Full Name" />
-        <Input type="about_me" name="about_me" label="About Me" />
-        <Input type="linkedin_link" name="linkedin_link" label="LinkedIn" />
-        <Input type="github_link" name="github_link" label="Github" />
-        <Input
-          type="role_description"
-          name="role_description"
-          label="Are you a bootcamper, exbootcamper or a mentor?"
-        />
-        <Input
-          type="skills"
-          name="skills"
-          label="Please give us a brief summary of you skills and the tech you have experience with"
-        />
-        <Button type="primary" htmlType="submit">
-          Log In
-        </Button>
-        <p>Forgotten Password</p>
-        <p>Here for the first time?</p>
-        <Button type="primary" htmlType="submit">
-          Create an Account
-        </Button>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
+
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" name="password" />
+
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" name="username" />
+
+        <label htmlFor="name">Full Name</label>
+        <input type="text" id="name" name="name" />
+
+        <label htmlFor="about_me">About Me</label>
+        <textarea id="about_me" name="about_me"></textarea>
+
+        <label htmlFor="linkedin_link">LinkedIn</label>
+        <input type="text" id="linkedin_link" name="linkedin_link" />
+
+        <label htmlFor="github_link">Github</label>
+        <input type="text" id="github_link" name="github_link" />
+
+        <label htmlFor="role_description">
+          Are you a bootcamper, exbootcamper or a mentor?
+        </label>
+        <input type="text" id="role_description" name="role_description" />
+
+        <label htmlFor="skills">
+          Please give us a brief summary of your skills and the tech you have
+          experience with
+        </label>
+        <textarea id="skills" name="skills"></textarea>
+
+        <button type="submit">Create my account</button>
+
+        {/* <button type="submit">Create an Account</button> */}
       </form>
     </div>
   );
