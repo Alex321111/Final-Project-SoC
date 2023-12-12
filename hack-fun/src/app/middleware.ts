f0ca6514a62945044bf9cd17a1ae19ad8d3f48f9
Selam Ararsa <selamkedir@icloud.com>
2023-12-12 14:28:29 +0000

    utils path resolved

--- hack-fun/src/app/middleware.ts
+++ hack-fun/src/app/middleware.ts
@@ -1,15 +1,15 @@
-'use client'
+'use client';
 
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import { NextResponse } from 'next/server'
-import supabase from '../utils/supabase'
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse } from 'next/server';
+import supabase from './utils/supabase';
 
-import type { NextRequest } from 'next/server'
-import type { Database } from './utils.types'
+import type { NextRequest } from 'next/server';
+import type { Database } from './utils/types';
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareClient<Database>({ req, res })
-  await supabase.auth.getSession()
-  return res
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient<Database>({ req, res });
+  await supabase.auth.getSession();
+  return res;
 }
