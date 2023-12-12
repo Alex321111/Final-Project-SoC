e3504d0c0e267378e6c1674ad2546c0b9aa734e0
Quinnlizzy <quin27@hotmail.co.uk>
2023-12-12 15:00:24 +0000

    profile tester

--- hack-fun/pages/profile.jsx
+++ hack-fun/pages/profile.jsx
@@ -2,6 +2,27 @@ import LeftSideBar from '../src/app/components/LeftSideBar';
 import Footer from '../src/app/components/Footer';
 import Header from '../src/app/components/Header';
 import BottomBar from '../src/app/components/BottomBar';
+import { createClient } from '@supabase/supabase-js'
+import supabase from '../src/app/utils/supabase'
+
+async function getUserProfile(userId) {
+  const { data, error } = await supabase
+    .from('user_profiles')
+    .select('name, username')
+    .eq('userid', userId)
+
+  if (error) {
+    console.error('Error: ', error)
+    return null
+  }
+
+  return data
+}
+// Usage
+getUserProfile('').then(userProfile => {
+  console.log(userProfile)
+})
+
 const UserProfile = () => {
   return (
     <>
