e20d17a2c4c8c2dd7b49765fb024d747c0b75aa0
Selam Ararsa <selamkedir@icloud.com>
2023-12-13 13:35:55 +0000

    form moved to components

--- hack-fun/pages/register.tsx
+++ hack-fun/pages/register.tsx
@@ -1,10 +1,10 @@
 //Register page tsx
 'use client';
-
+import '../styles/globals.css';
 import { useState } from 'react';
 import react from 'react';
 import CustomAvatar from '../src/app/components/Avatar.jsx';
-
+import CreateAccountForm from '../src/app/components/AccountForm.jsx';
 import supabase from '../src/app/utils/supabase';
 export default function Register() {
   const [avatarUrl, setAvatarUrl] = useState('');
@@ -65,47 +65,11 @@ export default function Register() {
       setAvatarUrl(userAvatarUrl);
     }
   };
+
   return (
-    <div className="mx-auto flex min-h-screen max-w-2xl items-center px-4">
-      <form className="w-full space-y-2" onSubmit={handleSubmit}>
-        <CustomAvatar size={40} url={avatarUrl} />
-
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
-
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" />
-
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
-
-        <label htmlFor="name">Full Name</label>
-        <input type="text" id="name" name="name" />
-
-        <label htmlFor="about_me">About Me</label>
-        <textarea id="about_me" name="about_me"></textarea>
-
-        <label htmlFor="linkedin_link">LinkedIn</label>
-        <input type="text" id="linkedin_link" name="linkedin_link" />
-
-        <label htmlFor="github_link">Github</label>
-        <input type="text" id="github_link" name="github_link" />
-
-        <label htmlFor="role_description">
-          Are you a bootcamper, exbootcamper or a mentor?
-        </label>
-        <input type="text" id="role_description" name="role_description" />
-
-        <label htmlFor="skills">
-          Please give us a brief summary of your skills and the tech you have
-          experience with
-        </label>
-        <textarea id="skills" name="skills"></textarea>
-
-        <button type="submit">Create my account</button>
-
-        {/* <button type="submit">Create an Account</button> */}
-      </form>
+    <div className="form-page">
+      <h1>Hello!</h1>
+      <CreateAccountForm />
     </div>
   );
 }
