eb31a8257e3565f31d3e0f80233d39e2f984e092
Thomas Wardle <twardle6@googlemail.com>
2023-12-13 15:28:19 +0000

    Profile Editing Working!

--- hack-fun/pages/profile.jsx
+++ hack-fun/pages/profile.jsx
@@ -1,29 +1,34 @@
-import LeftSideBar from '../src/app/components/LeftSideBar';
-import Footer from '../src/app/components/Footer';
-import Header from '../src/app/components/Header';
-import BottomBar from '../src/app/components/BottomBar';
-import { useCallback, useState, useEffect } from 'react';
-import react from 'react';
-import { Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs';
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import supabase from '../src/app/utils/supabase';
-import AccountForm from '../src/app/components/AccountForm';
+import LeftSideBar from "../src/app/components/LeftSideBar";
+import Footer from "../src/app/components/Footer";
+import Header from "../src/app/components/Header";
+import BottomBar from "../src/app/components/BottomBar";
+import supabase from "../src/app/utils/supabase";
+import AccountForm from "../src/app/components/AccountForm";
+import { useState, useEffect } from "react";
 
+export default function UserProfile() {
+	const [session, setSession] = useState(null);
 
+	useEffect(() => {
+		const { data: authListener } = supabase.auth.onAuthStateChange(
+			(event, session) => {
+				setSession(session);
+			}
+		);
 
+		// Cleanup function
+		return () => {
+			authListener.unsubscribe();
+		};
+	}, []);
 
-  export default async function UserProfile () {
-    const {
-      data: { session },
-    } = await supabase.auth.getSession()
-  return (
-    <>
-      {' '}
-      <Header />
-      <LeftSideBar />
-      <AccountForm session={session} />
-      <Footer />
-      <BottomBar />
-    </>
-  );
-};
+	return (
+		<>
+			<Header />
+			<LeftSideBar />
+			{session && <AccountForm session={session} />}
+			<Footer />
+			<BottomBar />
+		</>
+	);
+}
--- hack-fun/src/app/components/AccountForm.jsx
+++ hack-fun/src/app/components/AccountForm.jsx
@@ -1,108 +1,73 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import supabase from "../utils/supabase";
 
-'use client'
-import { useCallback, useEffect, useState } from 'react'
-import Avatar from './avatar'
-import { Database } from '../database.types'
-import { Session, createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+function AccountForm() {
+	const [user, setUser] = useState(null);
+	const [name, setName] = useState("");
+	const [username, setUsername] = useState("");
+	const [loading, setLoading] = useState(false); // Set initial loading state to false
 
-export default function AccountForm({ session }) {
-  const supabase = createClientComponentClient()
-  const [loading, setLoading] = useState(true)
-  const [name, setName] = useState(null)
-  const [username, setUsername] = useState(null)
-  const user = session?.user
+	useEffect(() => {
+		const { data: authListener } = supabase.auth.onAuthStateChange(
+			(event, session) => {
+				const currentUser = session?.user;
+				setUser(currentUser);
+				setName(currentUser?.user_metadata?.name || "");
+				setUsername(currentUser?.user_metadata?.username || "");
+			}
+		);
 
-  const getProfile = useCallback(async () => {
-    try {
-      setLoading(true)
+		return () => {
+			authListener.unsubscribe();
+		};
+	}, []);
 
-      const { data, error, status } = await supabase
-        .from('user_profiles')
-        .select(`name, username`)
-        .eq('id', user?.id)
-        .single()
+	const handleNameChange = (e) => {
+		setName(e.target.value);
+	};
 
-      if (error && status !== 406) {
-        throw error
-      }
+	const handleUsernameChange = (e) => {
+		setUsername(e.target.value);
+	};
 
-      if (data) {
-        setFullname(data.full_name)
-        setUsername(data.username)
-      }
-    } catch (error) {
-      alert('Error loading user data!')
-    } finally {
-      setLoading(false)
-    }
-  }, [user, supabase])
+	const handleSave = async () => {
+		setLoading(true);
+		console.log(user.id); // Debug log
+		console.log(name, username); // Debug log
+		const { error } = await supabase
+			.from("user_profiles")
+			.update({
+				name: name,
+				username: username,
+			})
+			.eq("id", user.id);
+		if (error) {
+			console.error(error);
+		} else setLoading(false);
+	};
 
-  useEffect(() => {
-    getProfile()
-  }, [user, getProfile])
-
-  async function updateProfile({ username }) {
-    try {
-      setLoading(true)
-
-      const { error } = await supabase.from('user_profiles').upsert({
-        id: user?.id,
-        full_name: fullname,
-        username,
-        updated_at: new Date().toISOString(),
-      })
-      if (error) throw error
-      alert('Profile updated!')
-    } catch (error) {
-      alert('Error updating the data!')
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  return (
-    <div className="form-widget">
-      <div>
-        <label htmlFor="email">Email</label>
-        <input id="email" type="text" value={session?.user.email} disabled />
-      </div>
-      <div>
-        <label htmlFor="name">Full Name</label>
-        <input
-          id="name"
-          type="text"
-          value={fullname || ''}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={username || ''}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-      </div>
-
-      <div>
-        <button
-          className="button primary block"
-          onClick={() => updateProfile({ name, username })}
-          disabled={loading}
-        >
-          {loading ? 'Loading ...' : 'Update'}
-        </button>
-      </div>
-
-      <div>
-        <form action="/auth/signout" method="post">
-          <button className="button block" type="submit">
-            Sign out
-          </button>
-        </form>
-      </div>
-    </div>
-  )
+	return (
+		<div>
+			<input
+				type="text"
+				value={name}
+				onChange={handleNameChange}
+				placeholder="Full Name"
+			/>
+			<input
+				type="text"
+				value={username}
+				onChange={handleUsernameChange}
+				placeholder="Username"
+			/>
+			<button
+				onClick={handleSave}
+				disabled={loading}>
+				Save
+			</button>
+		</div>
+	);
 }
+
+export default AccountForm;
