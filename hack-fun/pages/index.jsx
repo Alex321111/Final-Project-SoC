90d75cf82285d31902be34d66349ea97298caebc
Selam Ararsa <selamkedir@icloud.com>
2023-12-14 11:45:30 +0000

    log in form done

--- hack-fun/pages/index.jsx
+++ hack-fun/pages/index.jsx
@@ -20,64 +20,100 @@ const supabase = createClient(
 
 function SignInWithEmail() {
   const router = useRouter();
-
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   useEffect(() => {
     const { data: authListener } = supabase.auth.onAuthStateChange(
       (event, session) => {
         if (event === 'SIGNED_IN') {
           router.push('/home');
+        } else {
+          router.push('/');
         }
-        else {router.push('/');}
       }
     );
   }, []);
+  async function handleSignIn(e) {
+    e.preventDefault();
+    // try {
+    const { user, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    //   if (user) {
+    //     router.push('/home');
+    //   } else {
+    //     console.error(error);
+    //   }
+    // } catch (error) {
+    //   console.error(error);
+    // }
+  }
 
   return (
     <>
-    <div className="mr-6 mt-4">
-    <section className="flex flex-col md:flex-grow">
-      <div className="flex items-center justify-center">
-        <Image
-          className="hack-logo"
-          width={680}
-          height={120}
-          src={HackAFunLogo}
-          alt="hack-a-fun-logo"
-        />
-        </div>
+      <div className="mr-6 mt-4">
+        <section className="flex flex-col md:flex-grow">
+          <div className="flex items-center justify-center">
+            <Image
+              className="hack-logo"
+              width={680}
+              height={120}
+              src={HackAFunLogo}
+              alt="hack-a-fun-logo"
+            />
+          </div>
         </section>
-        </div>
-    <div className="bg-dark-2 max-w-4xl p-6 mx-auto rounded-md shadow-md mt-5">
-      <div className="min-h-screen flex items-center justify-center">
-        <Auth
-          supabaseClient={supabase}
-          socialLayout="horizontal"
-          styles={{
-            container: 'max-w-md w-full',
-            buttonPrimary: 'bg-green-500 hover:bg-green-600',
-            input: 'border rounded-md p-2 mb-2 w-full',
-            label: 'text-sm text-gray-600',
-            link: 'text-sm text-gray-600 hover:text-gray-800',}}
-            >
-            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-4">
-            <div className="flex space-x-2">
-              <button className="bg-blue-500 hover:bg-blue-600 px-3 py-2 rounded-md">Google</button>
-              <button className="bg-red-500 hover:bg-red-600 px-3 py-2 rounded-md">GitHub</button>
-              <button className="bg-red-500 hover:bg-red-600 px-3 py-2 rounded-md">Azure</button>
+      </div>
+
+      <div className="bg-dark-2 max-w-4xl p-6 mx-auto rounded-md shadow-md mt-5">
+        <div className="min-h-screen flex items-center justify-center">
+          <div className="w-full max-w-md">
+            <h2 className="text-2xl font-bold mb-4">Sign In</h2>
+
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700">
+                Email
+              </label>
+              <input
+                className="bg-dark-2"
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
+            <div className="mb-4">
+              <label htmlFor="password" className="block text-gray-700">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="bg-dark-2"
+                placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button
+              className="bg-blue-500 text-white px-4 py-2 rounded-md"
+              onClick={handleSignIn}
+            >
+              Sign In
+            </button>
           </div>
-          </Auth>
-          </div>
-          </div>
-      </>
-    );
-  }
+        </div>
+      </div>
+    </>
+  );
+}
 
-  export default SignInWithEmail;
+export default SignInWithEmail;
 
-
-
-  /*  <div className="bg-dark-2 max-w-4xl p-6 mx-auto rounded-md shadow-md mt-5">
+/*  <div className="bg-dark-2 max-w-4xl p-6 mx-auto rounded-md shadow-md mt-5">
       <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />
     </div>
   );
