f44e7695a3a1574a72173fe9eaaa349bed6244e3
Alex321111 <139920296+Alex321111@users.noreply.github.com>
2023-12-17 23:27:27 +0000

    styled the page and chatroom components using tailwind css

--- hack-fun/src/app/components/chatroom.jsx
+++ hack-fun/src/app/components/chatroom.jsx
@@ -52,29 +52,71 @@ const ChatRoom = () => {
 		}
 	};
 
+// 	return (
+// 		<div>
+// 			<ul>
+// 				{messages.map((message, index) => (
+// 					<li key={index}>
+// 						<p>
+// 							<strong>{message.username}</strong>: {message.message_content}
+// 						</p>
+// 						<p>{new Date(message.created_at).toLocaleString()}</p>
+// 					</li>
+// 				))}
+// 			</ul>
+// 			<form onSubmit={handleNewMessage}>
+// 				<input
+// 					type="text"
+// 					value={newMessage}
+// 					onChange={handleNewMessageChange}
+// 					placeholder="Type your message here..."
+// 				/>
+// 				<button type="submit">Send</button>
+// 			</form>
+// 		</div>
+// 	);
+// };
+  
 	return (
-		<div>
-			<ul>
-				{messages.map((message, index) => (
-					<li key={index}>
-						<p>
-							<strong>{message.username}</strong>: {message.message_content}
-						</p>
-						<p>{new Date(message.created_at).toLocaleString()}</p>
-					</li>
-				))}
-			</ul>
-			<form onSubmit={handleNewMessage}>
-				<input
-					type="text"
-					value={newMessage}
-					onChange={handleNewMessageChange}
-					placeholder="Type your message here..."
-				/>
-				<button type="submit">Send</button>
-			</form>
+	  <div className="bg-dark-2  h-screen flex justify flex-col">
+		<div className="flex-1 overflow-y-auto p-4">
+		  <ul className="space-y-4">
+			{messages.map((message, index) => (
+			  <li key={index} className="flex flex-col">
+				<div className="flex items-center justify-end mb-1">
+				  <span className="text-gray-600 mr-2">{message.username}</span>
+				  <span className="text-xs text-gray-400">
+					{new Date(message.created_at).toLocaleString()}
+				  </span>
+				</div>
+				<div className="flex items-center justify-end">
+				<div className="bg-white p-4 rounded shadow">
+				  <p className="text-gray-800">{message.message_content}</p>
+				</div>
+				</div>
+			  </li>
+			))}
+		  </ul>
 		</div>
+		<form onSubmit={handleNewMessage} className="p-4 bg-white">
+		  <div className="flex items-center justify-end">
+			<input
+			  type="text"
+			  value={newMessage}
+			  onChange={handleNewMessageChange}
+			  placeholder="Type your message here..."
+			  className="flex-1 justify-end border p-2 rounded-l focus:outline-none"
+			/>
+			<button
+			  type="submit"
+			  className="bg-blue-500 text-white py-2 px-4 rounded-r hover:bg-blue-600 focus:outline-none"
+			>
+			  Send
+			</button>
+		  </div>
+		</form>
+	  </div>
 	);
-};
-
-export default ChatRoom;
+  };
+  
+  export default ChatRoom;
