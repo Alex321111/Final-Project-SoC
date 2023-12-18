9e7ae088d2b644bf6f01a948a79f370f22f15008
Alex321111 <139920296+Alex321111@users.noreply.github.com>
2023-12-18 16:23:40 +0000

    project submission form functionality and styling

--- hack-fun/tailwind.config.ts
+++ hack-fun/tailwind.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  
+  // purge: ["./src/pages/**/*.{js,jsx,ts,tsx}", './src/styles/**/*.css'],
   theme: {
     container: {
       center: true,
