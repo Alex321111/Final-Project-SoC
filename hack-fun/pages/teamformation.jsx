10ea8606cd736e4c75ba1ae34a32e3e58ec660e9
Thomas Wardle <twardle6@googlemail.com>
2023-12-18 16:47:05 +0000

    Now able to get hold of mentors and bootcampers

--- hack-fun/pages/teamformation.jsx
+++ hack-fun/pages/teamformation.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import createTeams from '../src/app/components/teamformation'
-const handleClick = () => {
-    createTeams().catch(console.error);
+import React from "react";
+import createTeams from "../src/app/components/teamformation";
+const handleClick = async () => {
+	await createTeams();
 };
 
 export default function teamformation() {
-    return (
-        <div>
-            <div>
-                <h1>Create Teams</h1>
-                <button onClick={handleClick}>Create Teams</button>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<div>
+				<h1>Create Teams</h1>
+				<button onClick={handleClick}>Create Teams</button>
+			</div>
+		</div>
+	);
 }
--- hack-fun/src/app/components/teamformation.jsx
+++ hack-fun/src/app/components/teamformation.jsx
@@ -2,48 +2,79 @@ import supabase from "../utils/supabase";
 import React from "react";
 
 async function createTeams() {
+	console.log("createTeams called");
 	// Fetch mentors and non-mentors
-	const { data: mentors, error: mentorsError } = await supabase.rpc(
+	// let { data: mentors, error: mentorsError } = await supabase.rpc(
+	// 	"get_mentors"
+	// );
+	// if (mentorsError) {
+	// 	console.error("Error fetching mentors: ", mentorsError);
+	// 	return;
+	// }
+	// console.log(mentors);
+
+	let { data: mentorsData, error: mentorsError } = await supabase.rpc(
 		"get_mentors"
 	);
 	if (mentorsError) {
-		console.error("Error fetching mentors: ", mentorsError);
+		console.error("Error calling get_mentors:", mentorsErrorrror);
 		return;
 	}
 
-	const { data: nonMentors, error: nonMentorsError } = await supabase.rpc(
+	let { data: nonmentorsData, error: nonmentorsError } = await supabase.rpc(
 		"get_nonmentors"
 	);
-	if (nonMentorsError) {
-		console.error("Error fetching non-mentors: ", nonMentorsError);
+	if (nonmentorsError) {
+		console.error("Error calling get_mentors:", nonmentorsError);
 		return;
 	}
-	// Create a team for each mentor and store the team ids
-	const teamIds = await Promise.all(
-		mentors.map(async (mentor) => {
-			const { data: team, error } = await supabase
-				.from("all_teams")
-				.insert([{ user_id: mentor.id }]);
-			console.log(mentor.id).single();
-			if (error) {
-				console.error("Error inserting team: ", error);
-				return;
-			}
-			return team.id;
-		})
-	);
-	// Assign non-mentors to teams in a round-robin fashion
-	for (let i = 0; i < nonMentors.length; i++) {
-		const teamId = teamIds[i % teamIds.length];
-		const { error } = await supabase
-			.from("all_teams")
-			.update({ team_id: teamId })
-			.eq("user_id", nonMentors[i].id);
-		if (error) {
-			console.error("Error updating team_id: ", error);
-		}
-	}
 
-	createTeams().catch(console.error);
+	console.log("get_mentors returned:", mentorsData);
+	console.log("get_nonmentors returned:", nonmentorsData);
+
+	// data.forEach((mentor) => {
+	// 	let userId = mentor.user_id;
+	// 	let roleDescription = mentor.role_description;
+	// 	console.log(mentor);
+	// Use userId and roleDescription in your team forming functions
+	// });
+
+	// const { data: nonMentors, error: nonMentorsError } = await supabase.rpc(
+	// 	"get_nonmentors"
+	// );
+	// if (nonMentorsError) {
+	// 	console.error("Error fetching non-mentors: ", nonMentorsError);
+	// 	return;
+	// }
+	// console.log("Mentors:", mentors);
+	// console.log("Non-mentors:", nonMentors);
+
+	// const teamIds = await Promise.all(
+	// 	mentors.map(async (mentor) => {
+	// 		console.log("Processing mentor:", mentor);
+	// 		const { data: team, error } = await supabase
+	// 			.from("all_teams")
+	// 			.insert([{ user_id: mentor.id }]);
+	// 		console.log(mentor.id).single();
+	// 		if (error) {
+	// 			console.error("Error inserting team: ", error);
+	// 			return;
+	// 		}
+	// 		return team.id;
+	// 	})
+	// );
+	// Assign non-mentors to teams in a round-robin fashion
+	// for (let i = 0; i < nonMentors.length; i++) {
+	// 	const teamId = teamIds[i % teamIds.length];
+	// 	const { error } = await supabase
+	// 		.from("all_teams")
+	// 		.update({ team_id: teamId })
+	// 		.eq("user_id", nonMentors[i].id);
+	// 	if (error) {
+	// 		console.error("Error updating team_id: ", error);
+	// 	}
+	// }
+
+	// createTeams().catch(console.error);
 }
 export default createTeams;
