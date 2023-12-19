876eb73976f20a06ce082eaf95806bd828f1c504
Selam Ararsa <selamkedir@icloud.com>
2023-12-19 10:05:24 +0000

    error in groupchat

--- hack-fun/pages/groupchat.jsx
+++ hack-fun/pages/groupchat.jsx
@@ -2,19 +2,17 @@ import LeftSideBar from '../src/app/components/LeftSideBar';
 import HomeIntro from '../src/app/components/HomeIntro';
 import Footer from '../src/app/components/Footer';
 import Header from '../src/app/components/Header';
-import ChatRoom from '../src/app/components/chatroom';
+import ChatComponent from '../src/app/components/ChatComponent';
 
-const GroupChat = () => 
-
-	return (
-		<>
-			<Header />
-			<LeftSideBar />
-			<h1></h1>
-			<ChatRoom />
-			<Footer />
-		</>
-	);
-
+const GroupChat = () => {
+  return (
+    <>
+      <Header />
+      <LeftSideBar />
+      <h1></h1>
+      <ChatComponent />
+      <Footer />
+    </>
+  );
 };
 export default GroupChat;
