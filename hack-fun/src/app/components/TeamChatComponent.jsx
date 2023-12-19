cf02214236456529b931692000c4fbb590dae67b
Thomas Wardle <twardle6@googlemail.com>
2023-12-19 17:21:19 +0000

    Payload Filter Added

--- hack-fun/src/app/components/TeamChatComponent.jsx
+++ hack-fun/src/app/components/TeamChatComponent.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import  supabase  from '../utils/supabase';
+import React, { useEffect, useState } from "react";
+import supabase from "../utils/supabase";
 // Function to fetch team ID
 
 /*async function fetchTeamId(userId) {
@@ -37,10 +37,6 @@ async function fetchTeamMessages(teamId) {
   }
   return messages;
 }*/
-
-
-
-
 
 // ChatRoom component
 /*
@@ -172,8 +168,6 @@ async function fetchTeamMessages(teamId) {
 };
 */
 
-
-
 //our version
 /*const TeamChatRoom = () => {
   const [user, setUser] = useState(null);
@@ -281,118 +275,119 @@ async function fetchTeamMessages(teamId) {
   );
 };*/
 
-
-
-
-
-
 //co-pilot suggestion
 
 const TeamChatRoom = () => {
-  const [user, setUser] = useState(null);
-  const [newMessage, setNewMessage] = useState("");
-  const [messages, setMessages] = useState([]);
-  const [teamId, setTeamId] = useState(null);
-  useEffect(() => {
-      const { data: authListener } = supabase.auth.onAuthStateChange(
-          async (event, session) => {
-              const currentUser = session?.user;
-              setUser(currentUser);
-              if (currentUser) {
-                  const { data, error } = await supabase
-                      .from('all_teams')
-                      .select('team_id')
-                      .eq('user_id', currentUser.id)
-                      .single();
-                  if (error) {
-                      console.error('Error fetching team ID:', error);
-                      return;
-                  }
-                  setTeamId(data.team_id);
-              }
-          }
-      );
-  }, []);
-  useEffect(() => {
-      if (teamId) {
-          fetchMessages();
-          const generalChat = supabase
-              .channel("general_chat")
-              .on(
-                  "postgres_changes",
-                  { event: "INSERT", schema: "public", table: "general_chat" },
-                  (payload) => {
-                      console.log("Change received!", payload);
-                      setMessages((prevMessages) => [...prevMessages, payload.new]);
-                  }
-              )
-              .subscribe();
-      }
-  }, [teamId]);
-  const fetchMessages = async () => {
-      const { data, error } = await supabase.from("general_chat").select("*").eq('team_id', teamId);
-      if (error) console.error("Error fetching messages: ", error);
-      else setMessages(data);
-  };
-  const handleNewMessageChange = (event) => {
-      setNewMessage(event.target.value);
-  };
-  const handleNewMessage = async (event) => {
-      event.preventDefault();
-      if (newMessage.trim() === "") return;
-      if (user && teamId) {
-          const { data, error } = await supabase
-              .from("general_chat")
-              .insert([{ message_content: newMessage, user_id: user.id, team_id: teamId }]);
-          if (error) {
-              console.error("Error inserting message: ", error);
-          } else {
-              setNewMessage("");
-          }
-      }
-  };
-  return (
-      <div className="bg-dark-2  h-screen flex justify flex-col">
-          <div className="flex-1 overflow-y-auto p-4">
-              <ul className="space-y-4">
-                  {messages.map((message, index) => (
-                      <li
-                          key={index}
-                          className="flex flex-col">
-                          <div className="flex items-center justify-end mb-1">
-                              <span className="text-gray-600 mr-2">{message.username}</span>
-                              <span className="text-xs text-gray-400">
-                                  {new Date(message.created_at).toLocaleString()}
-                              </span>
-                          </div>
-                          <div className="flex items-center justify-end">
-                              <div className="bg-white p-4 rounded shadow">
-                                  <p className="text-gray-800">{message.message_content}</p>
-                              </div>
-                          </div>
-                      </li>
-                  ))}
-              </ul>
-          </div>
-          <form onSubmit={handleNewMessage}>
-              <div className="flex items-center justify-end">
-                  <input
-                      type="text"
-                      value={newMessage}
-                      onChange={handleNewMessageChange}
-                      placeholder="Type your message here..."
-                      className="flex-1 border p-2 rounded-l focus:outline-none"
-                  />
-                  <button
-                      type="submit"
-                      className="bg-blue-500 text-white py-2 px-4 rounded-r hover:bg-blue-600 focus:outline-none">
-                      Send
-                  </button>
-              </div>
-          </form>
-      </div>
-  );
+	const [user, setUser] = useState(null);
+	const [newMessage, setNewMessage] = useState("");
+	const [messages, setMessages] = useState([]);
+	const [teamId, setTeamId] = useState(null);
+	useEffect(() => {
+		const { data: authListener } = supabase.auth.onAuthStateChange(
+			async (event, session) => {
+				const currentUser = session?.user;
+				setUser(currentUser);
+				if (currentUser) {
+					const { data, error } = await supabase
+						.from("all_teams")
+						.select("team_id")
+						.eq("user_id", currentUser.id)
+						.single();
+					if (error) {
+						console.error("Error fetching team ID:", error);
+						return;
+					}
+					setTeamId(data.team_id);
+				}
+			}
+		);
+	}, []);
+	useEffect(() => {
+		if (teamId) {
+			fetchMessages();
+			const generalChat = supabase
+				.channel("general_chat")
+				.on(
+					"postgres_changes",
+					{ event: "INSERT", schema: "public", table: "general_chat" },
+					(payload) => {
+						console.log("Change received!", payload);
+						if (payload.new.team_id === teamId) {
+							setMessages((prevMessages) => [...prevMessages, payload.new]);
+						}
+					}
+				)
+				.subscribe();
+		}
+	}, [teamId]);
+	const fetchMessages = async () => {
+		const { data, error } = await supabase
+			.from("general_chat")
+			.select("*")
+			.eq("team_id", teamId);
+		if (error) console.error("Error fetching messages: ", error);
+		else setMessages(data);
+	};
+	const handleNewMessageChange = (event) => {
+		setNewMessage(event.target.value);
+	};
+	const handleNewMessage = async (event) => {
+		event.preventDefault();
+		if (newMessage.trim() === "") return;
+		if (user && teamId) {
+			const { data, error } = await supabase
+				.from("general_chat")
+				.insert([
+					{ message_content: newMessage, user_id: user.id, team_id: teamId },
+				]);
+			if (error) {
+				console.error("Error inserting message: ", error);
+			} else {
+				setNewMessage("");
+			}
+		}
+	};
+	return (
+		<div className="bg-dark-2  h-screen flex justify flex-col">
+			<div className="flex-1 overflow-y-auto p-4">
+				<ul className="space-y-4">
+					{messages.map((message, index) => (
+						<li
+							key={index}
+							className="flex flex-col">
+							<div className="flex items-center justify-end mb-1">
+								<span className="text-gray-600 mr-2">{message.username}</span>
+								<span className="text-xs text-gray-400">
+									{new Date(message.created_at).toLocaleString()}
+								</span>
+							</div>
+							<div className="flex items-center justify-end">
+								<div className="bg-white p-4 rounded shadow">
+									<p className="text-gray-800">{message.message_content}</p>
+								</div>
+							</div>
+						</li>
+					))}
+				</ul>
+			</div>
+			<form onSubmit={handleNewMessage}>
+				<div className="flex items-center justify-end">
+					<input
+						type="text"
+						value={newMessage}
+						onChange={handleNewMessageChange}
+						placeholder="Type your message here..."
+						className="flex-1 border p-2 rounded-l focus:outline-none"
+					/>
+					<button
+						type="submit"
+						className="bg-blue-500 text-white py-2 px-4 rounded-r hover:bg-blue-600 focus:outline-none">
+						Send
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 };
-
 
 export default TeamChatRoom;
