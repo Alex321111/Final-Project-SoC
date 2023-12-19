c314a08e1561d23ddad02df2d352fd2848fbbfa6
Selam Ararsa <selamkedir@icloud.com>
2023-12-19 09:56:07 +0000

    project submission page

--- hack-fun/pages/groupchat.jsx
+++ hack-fun/pages/groupchat.jsx
@@ -1,18 +1,18 @@
-import LeftSideBar from "../src/app/components/LeftSideBar";
-import HomeIntro from "../src/app/components/HomeIntro";
-import Footer from "../src/app/components/Footer";
-import Header from "../src/app/components/Header";
-import ChatRoom from "../src/app/components/chatroom";
+import LeftSideBar from '../src/app/components/LeftSideBar';
+import HomeIntro from '../src/app/components/HomeIntro';
+import Footer from '../src/app/components/Footer';
+import Header from '../src/app/components/Header';
+import ChatRoom from '../src/app/components/chatroom';
 
 const GroupChat = () => {
-	return (
-		<>
-			<Header />
-			<LeftSideBar />
-			<h1>You are in the group chat page !</h1>
-			<ChatRoom />
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <LeftSideBar />
+      <h1>You are in the group chat page !</h1>
+      <ChatRoom />
+      <Footer />
+    </>
+  );
 };
 export default GroupChat;
