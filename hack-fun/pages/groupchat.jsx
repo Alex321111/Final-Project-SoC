9681dfc84bcc6baab2c3046355b9842d0afa162c
Selam Ararsa <selamkedir@icloud.com>
2023-12-19 11:44:13 +0000

    community page

--- hack-fun/pages/groupchat.jsx
+++ hack-fun/pages/groupchat.jsx
@@ -2,17 +2,41 @@ import LeftSideBar from '../src/app/components/LeftSideBar';
 import HomeIntro from '../src/app/components/HomeIntro';
 import Footer from '../src/app/components/Footer';
 import Header from '../src/app/components/Header';
-import ChatComponent from '../src/app/components/ChatComponent';
 
-const GroupChat = () => {
+import BottomBar from '../src/app/components/BottomBar';
+import Chatroom from '../src/app/components/chatroom';
+const GroupChat = ({ userName }) => {
   return (
     <>
-      <Header />
-      <LeftSideBar />
-      <h1></h1>
-      <ChatComponent />
-      <Footer />
+      <div className="flex flex-col min-h-screen ">
+        <div className="flex flex-grow h-full">
+          <div className="left-side-bar">
+            <LeftSideBar userName={userName} />
+          </div>
+
+          <section className="flex  items-center justify-center flex-col md:flex-grow">
+            <Chatroom />
+          </section>
+        </div>
+      </div>
+
+      <div className="bottom-bar-container">
+        <BottomBar userName={userName} />
+      </div>
     </>
   );
 };
+
 export default GroupChat;
+{
+  /* <div className="flex flex-col flex-grow w-full">
+        <section className="flex flex-col flex-grow w-full max-w-xl bg-dark-2 shadow-xl rounded-lg overflow-hidden">
+          <ChatComponent />
+        </section>
+
+        <div className="bottom-bar-container flex-shrink-0">
+          <BottomBar userName={userName} />
+        </div>
+      </div>
+    </div> */
+}
