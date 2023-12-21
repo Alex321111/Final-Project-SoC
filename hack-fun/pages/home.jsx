ed4cf950e83753f34133594003e4d0f24b4808df
Julian Ngo <julian@Julians-iMac.local>
2023-12-21 14:43:35 +0000

    added stlying changes to left side bar

--- hack-fun/pages/home.jsx
+++ hack-fun/pages/home.jsx
@@ -1,28 +1,28 @@
-'use client';
-import LeftSideBar from '../src/app/components/LeftSideBar';
-import HomeIntro from '../src/app/components/HomeIntro';
+"use client";
+import LeftSideBar from "../src/app/components/LeftSideBar";
+import HomeIntro from "../src/app/components/HomeIntro";
 // import HomeCard from '../src/app/components/HomeCard';
-import Footer from '../src/app/components/Footer';
-import Header from '../src/app/components/Header';
-import BottomBar from '../src/app/components/BottomBar';
-import Link from 'next/link';
-import Image from 'next/image';
-import '../styles/globals.css';
-import HackAFunLogo from '../src/app/components/hack-a-fun.png';
-import CustomAvatar from '../src/app/components/Avatar';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import supabase from '../src/app/utils/supabase';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import CountDownTimer from '../src/app/components/Countdown';
-import dynamic from 'next/dynamic';
-const HomeCard = dynamic(() => import('../src/app/components/HomeCard'), {
+import Footer from "../src/app/components/Footer";
+import Header from "../src/app/components/Header";
+import BottomBar from "../src/app/components/BottomBar";
+import Link from "next/link";
+import Image from "next/image";
+import "../styles/globals.css";
+import HackAFunLogo from "../src/app/components/hack-a-fun.png";
+import CustomAvatar from "../src/app/components/Avatar";
+import { useEffect } from "react";
+import { useState } from "react";
+import supabase from "../src/app/utils/supabase";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import CountDownTimer from "../src/app/components/Countdown";
+import dynamic from "next/dynamic";
+const HomeCard = dynamic(() => import("../src/app/components/HomeCard"), {
   ssr: false,
 });
 
 const Home = () => {
-  const notify = () => toast('Wow so easy!');
-  const [userName, setUserName] = useState('');
+  const notify = () => toast("Wow so easy!");
+  const [userName, setUserName] = useState("");
   useEffect(() => {
     const { authListener } = supabase.auth.onAuthStateChange(
       (event, session) => {
@@ -31,7 +31,7 @@ const Home = () => {
           setUserName(currentUser.user_metadata?.username);
           console.log(userName);
         } else {
-          setUserName('Guest');
+          setUserName("Guest");
         }
       }
     );
@@ -48,7 +48,7 @@ const Home = () => {
             <section className="flex flex-col md:flex-grow">
               <div className="flex items-end justify-center ">
                 <Image
-                  className="hack-logo"
+                  className="hack-logo m-10"
                   width={380}
                   height={80}
                   src={HackAFunLogo}
@@ -59,10 +59,6 @@ const Home = () => {
               <HomeIntro userName={userName} />
               <HomeCard />
             </section>
-            <button onClick={notify} className="p-7">
-              {' '}
-              Hello{' '}
-            </button>
           </div>
         </div>
       </div>
