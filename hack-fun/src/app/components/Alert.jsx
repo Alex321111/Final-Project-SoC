449a5059be76ddd8bd87899edb4360ece2c262ac
Selam Ararsa <selamkedir@icloud.com>
2023-12-21 21:22:47 +0000

    working on bottom bar

--- hack-fun/src/app/components/Alert.jsx
+++ hack-fun/src/app/components/Alert.jsx
@@ -1,13 +1,13 @@
 const Alert = ({ message, type, onClose }) => (
   <div
-    className={`fixed top-0 bg-opacity-100 left-0 right-0 bg-${type}-900 text-center py-4 lg:px-4 bg-dark-2`}
+    className={`fixed top-0 bg-opacity-100 left-0 right-0 bg-${type}-900 text-center py-4 lg:px-4 bg-[#009b49]`}
   >
     <div
       className={`p-2 bg-${type}-800 items-center text-${type}-100 leading-none lg:rounded-full flex lg:inline-flex`}
       role="alert"
     >
       <span
-        className={`flex rounded-full bg-${type}-500 uppercase px-2 py-1 text-xs font-bold mr-3`}
+        className={`flex rounded-full bg-yellow-500 uppercase px-2 py-1 text-xs font-bold mr-3`}
       >
         New
       </span>
