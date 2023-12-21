ece191ac6aaa22da37c6ffad279bcb0191c29a9d
Quinnlizzy <quin27@hotmail.co.uk>
2023-12-21 22:45:26 +0000

    mvp

--- hack-fun/src/app/components/ProjectCard.jsx
+++ hack-fun/src/app/components/ProjectCard.jsx
@@ -27,6 +27,51 @@
       <>
         {projects.map((project, index) => (
           <div key={index} className="max-w-2xl mx-auto">
+            <div className="m-2 bg-dark-2 shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+              <a href={project.presentation_link}>
+                <img
+                  className="rounded-t-lg"
+                  src="https://images.unsplash.com/photo-1633356122544-f134324a6cee?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="ReactProject"
+                />
+              </a>
+              <div className="p-5">
+                <a href={project.presentation_link}>
+                  <h5 className="text-blue-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+                    {project.team_name}
+                  </h5>
+                </a>
+                <p className="font-normal text-white mb-3 dark:text-gray-400">
+                  {project.project_description}
+                </p>
+                <div className="flex justify-between space-x-4">
+  <a
+    href={project.github_project_link}
+    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  >
+    Github Project Link
+  </a>
+  <a
+    href={project.presentation_link}
+    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+  >
+    Presentation Link
+  </a>
+</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </>
+    );
+
+
+/*
+
+    return (
+      <>
+        {projects.map((project, index) => (
+          <div key={index} className="max-w-2xl mx-auto">
             <div className=" m-2 bg-dark-2 shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
               <a href={project.presentation_link}>
                 <img
@@ -56,6 +101,10 @@
         ))}
       </>
     );
+*/
+
+
+
 
 /*return (
         <div>
