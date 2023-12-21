cdcb26ba5813627be5e68d09c5550e82a7a0f58e
Quinnlizzy <quin27@hotmail.co.uk>
2023-12-21 22:15:58 +0000

    projfeed

--- hack-fun/src/app/components/ProjectCard.jsx
+++ hack-fun/src/app/components/ProjectCard.jsx
@@ -1,5 +1,43 @@
-export default function ProjectCard({ userName }) {
-  //comment
+"use client";
+  import React, { useState, useEffect } from "react";
+  import supabase from "../utils/supabase";
+  
+    export default function ProjectCard() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        fetchProjects();
+    }, []);
+
+    const fetchProjects = async () => {
+        const { data, error } = await supabase
+            .from('project_feed')
+            .select('*');
+
+        if (error) {
+            console.error('Error: ', error);
+        } else {
+            console.log('Projects fetched successfully: ', data);
+            setProjects(data);
+        }
+    };
+
+   /* return (
+        <div>
+            {projects.map((project, index) => (
+                <div key={index}>
+                    <h2>{project.username}</h2>
+                    <p>{project.team_name}</p>
+                    <a href={project.github_project_link}>Github Project Link</a>
+                    <p>{project.project_description}</p>
+                    <a href={project.presentation_link}>Presentation Link</a>
+                </div>
+            ))}
+        </div>
+    );
+}*/
+
+
   return (
     <>
       <div>
@@ -15,20 +53,18 @@ export default function ProjectCard({ userName }) {
             <div className="p-5">
               <a href="#">
                 <h5 className="text-blue-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
-                  React SPA Hackathon
+                  team_name
                 </h5>
               </a>
               <p className="font-normal text-white mb-3 dark:text-gray-400">
-                Welcome to the React SPA (Single Page Application) App
-                Hackathon! This challenge is a fantastic opportunity to harness
-                your React skills and understanding of component-based
-                architecture.
+                project_description
+                presentation_link
               </p>
               <a
                 href="https://docs.google.com/document/d/e/2PACX-1vSC4-YfIvlgggRmPM-jlvKjF_o41ZBYcraKvO-8AKeEMhyeS4yMmCaSf9pyavsw3MzpHPvONz-OZIhj/pub"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                Access Project Brief
+                github_project_link
               </a>
             </div>
           </div>
@@ -43,6 +79,7 @@ export default function ProjectCard({ userName }) {
         </div>
       </div>
 
+
       <div className="max-w-2xl mx-auto">
         <div className="bg-dark-2 shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
@@ -55,17 +92,18 @@ export default function ProjectCard({ userName }) {
           <div className="p-5">
             <a href="#">
               <h5 className="text-blue-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
-                Fetch API Hackathon
+                team_name
               </h5>
             </a>
             <p className="font-normal text-white mb-3 dark:text-gray-400">
-              Test your skills on fetching an API!
+              project_description
+              presentation_link
             </p>
             <a
               href="https://github.com/SchoolOfCode/workshop_fetch-challenge"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
-              Access Project Brief
+              github_project_link
             </a>
           </div>
         </div>
@@ -79,6 +117,36 @@ export default function ProjectCard({ userName }) {
         </p>
       </div>
 
+
+
+
+      <div className="max-w-2xl mx-auto">
+        <div className="bg-dark-2 shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+          <a href="#">
+            <img
+              className="rounded-t-lg"
+              src="https://images.unsplash.com/photo-1504805572947-34fad45aed93?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+            />
+          </a>
+          <div className="p-5">
+            <a href="https://docs.google.com/document/d/e/2PACX-1vRa4v7rAZWZpBs0R_kH_mPpPl788cY9wDILKJkJL6ieKrU2jtsK_9demcudSFDFSU5uAQ30TEM9ENt8/pub">
+              <h5 className="text-blue-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+                team_name
+              </h5>
+            </a>
+            <p className="font-normal text-white mb-3 dark:text-gray-400">
+              project_description
+            </p>
+            <a
+              href="https://docs.google.com/document/d/e/2PACX-1vRa4v7rAZWZpBs0R_kH_mPpPl788cY9wDILKJkJL6ieKrU2jtsK_9demcudSFDFSU5uAQ30TEM9ENt8/pub"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Access Project Brief
+            </a>
+          </div>
+        </div>
+      </div>
       <div className="max-w-2xl mx-auto">
         <div className="bg-dark-2 shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
           <a href="#">
