187e8f8096f2497d13a91e217790e910c7db82e8
Quinnlizzy <quin27@hotmail.co.uk>
2023-12-21 15:06:38 +0000

    playwrighttestingworking

---
+++ hack-fun/tests/test-2.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3001/');
+  await page.getByLabel('Email').click();
+  await page.getByLabel('Email').fill('quin27@hotmail.co.uk');
+  await page.getByLabel('Email').press('Tab');
+  await page.getByLabel('Password').fill('scampi');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  expect(page.getByRole('heading', { name: 'Hackathon' })).toBeVisible();
+  await page.getByRole('link', { name: 'Community chat' }).click();
+  expect(page.getByRole('button', { name: 'Send' }));
+  await page.getByRole('link', { name: 'Home' }).click();
+  expect(page.getByRole('link', { name: 'Read more 🚀' }));
+  await page.getByRole('button', { name: 'Sign Up' }).click();
+  expect(page.getByText('You have signed up !'));
+  await page.getByRole('img', { name: 'Close' }).click();
+  await page.getByRole('link', { name: 'Community chat' }).click();
+  await page.getByPlaceholder('Type your message here...').click();
+  await page.getByPlaceholder('Type your message here...').click();
+  await page.getByPlaceholder('Type your message here...').fill('hello again');
+  await page.getByRole('button', { name: 'Send' }).click();
+  await page.getByRole('navigation').locator('div').filter({ hasText: 'HomeCommunity chatTeam' }).locator('div').nth(4).click();
+});
