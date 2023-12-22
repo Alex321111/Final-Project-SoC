b65b2794599ca585ab6d42333b108cb2dc785c80
Alex321111 <139920296+Alex321111@users.noreply.github.com>
2023-12-22 14:02:15 +0000

    past projects page styling

--- hack-fun/src/app/components/ProjectCard.jsx
+++ hack-fun/src/app/components/ProjectCard.jsx
@@ -55,7 +55,7 @@
     href={project.presentation_link}
     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
   >
-    Presentation Link
+    Projects Presentation Link
   </a>
 </div>
               </div>
