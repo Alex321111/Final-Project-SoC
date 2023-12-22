acdda259dcd1d56704353e388bd09d6439f35aa6
Selam Ararsa <selamkedir@icloud.com>
2023-12-22 12:21:34 +0000

    added a badge to profile

--- hack-fun/pages/profile.jsx
+++ hack-fun/pages/profile.jsx
@@ -1,12 +1,12 @@
-import LeftSideBar from "../src/app/components/LeftSideBar";
-import Footer from "../src/app/components/Footer";
-import Header from "../src/app/components/Header";
-import BottomBar from "../src/app/components/BottomBar";
-import supabase from "../src/app/utils/supabase";
-import AccountForm from "../src/app/components/AccountForm";
-import { useState, useEffect } from "react";
-import "../styles/globals.css";
-import ProfilePage from "../src/app/components/profilePage";
+import LeftSideBar from '../src/app/components/LeftSideBar';
+import Footer from '../src/app/components/Footer';
+import Header from '../src/app/components/Header';
+import BottomBar from '../src/app/components/BottomBar';
+import supabase from '../src/app/utils/supabase';
+import AccountForm from '../src/app/components/AccountForm';
+import { useState, useEffect } from 'react';
+import '../styles/globals.css';
+import ProfilePage from '../src/app/components/profilePage';
 
 export default function UserProfile({ userName }) {
   const [session, setSession] = useState(null);
@@ -34,7 +34,7 @@ export default function UserProfile({ userName }) {
           </div>
           <section className="flex  items-center justify-center flex-col md:flex-grow">
             <ProfilePage />
-            {session && <AccountForm session={session} />}
+            {/* {session && <AccountForm session={session} />} */}
           </section>
         </div>
       </div>
